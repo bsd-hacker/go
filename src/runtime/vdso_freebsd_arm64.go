4cc873cabd8491dbbcdb7ebfb7934348bc49f9d1
Tobias Klauser <tklauser@distanz.ch>
2018-11-27 20:40:34 +0100

    runtime: add support for freebsd/arm64
    Change-Id: If3189a693ca0aa627029e22b0f91534bcf322bc0

---
+++ src/runtime/vdso_freebsd_arm64.go
@@ -0,0 +1,13 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package runtime
+
+//go:nosplit
+func (th *vdsoTimehands) getTimecounter() (uint32, bool) {
+	switch th.algo {
+	default:
+		return 0, false
+	}
+}
