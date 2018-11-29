07c98e8f9c382d04327c7d8c38756eebc9637f02
Tobias Klauser <tklauser@distanz.ch>
2018-11-29 14:01:23 +0100

    runtime: implement archauxv on freebsd/arm64
    Change-Id: Iaaca2d74ffbf1f6eda6a724892666f03c22d1937

--- src/runtime/os_freebsd_arm64.go
+++ src/runtime/os_freebsd_arm64.go
@@ -4,6 +4,21 @@
 
 package runtime
 
+import _ "unsafe" // for go:linkname
+
+// arm64 doesn't have a 'cpuid' instruction equivalent and relies on
+// HWCAP/HWCAP2 bits for hardware capabilities.
+
+//go:linkname cpu_hwcap internal/cpu.hwcap
+var cpu_hwcap uint
+
+func archauxv(tag, val uintptr) {
+	switch tag {
+	case _AT_HWCAP:
+		cpu_hwcap = uint(val)
+	}
+}
+
 //go:nosplit
 func cputicks() int64 {
 	// Currently cputicks() is used in blocking profiler and to seed runtime·fastrand().
