f428c6e94e3987fde73397269178c604fd3542ff
Dmitri Goutnik <dg@syrec.org>
2019-04-26 12:49:18 -0500

    runtime: add setNsec

--- src/runtime/defs_freebsd_arm64.go
+++ src/runtime/defs_freebsd_arm64.go
@@ -185,8 +185,9 @@ type timespec struct {
 	tv_nsec int64
 }
 
-func (ts *timespec) set_sec(x int64) {
-	ts.tv_sec = x
+func (ts *timespec) setNsec(ns int64) {
+	ts.tv_sec = ns / 1e9
+	ts.tv_nsec = ns % 1e9
 }
 
 type timeval struct {
