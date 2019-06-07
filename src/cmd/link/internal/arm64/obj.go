51fc12a4bd64dbe7f6c69932b7f5378c8f65b921
YAMAMOTO, Shigeru <shigeru@os-hackers.jp>
2019-06-07 14:01:34 +0900

    fix syntax error, it is miss at self merging from dmgk/freebsd-arm64

--- src/cmd/link/internal/arm64/obj.go
+++ src/cmd/link/internal/arm64/obj.go
@@ -86,7 +86,7 @@ func archinit(ctxt *ld.Link) {
 
 	case objabi.Hlinux, /* arm64 elf */
 		objabi.Hnetbsd,
-		objabi.Hopenbsd:
+		objabi.Hopenbsd,
 		objabi.Hfreebsd:
 		ld.Elfinit(ctxt)
 		ld.HEADR = ld.ELFRESERVE
