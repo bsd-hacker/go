86ed36d5002f32146fa01995e722f9eb574857be
YAMAMOTO, Shigeru <shigeru@os-hackers.jp>
2019-06-27 11:46:44 +0900

    add archauxv for freebsd/arm64.

--- src/runtime/os_freebsd_arm64.go
+++ src/runtime/os_freebsd_arm64.go
@@ -2,7 +2,22 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build arm64
+
 package runtime
+
+import "internal/cpu"
+
+func archauxv(tag, val uintptr) {
+	switch tag {
+	case _AT_HWCAP:
+		// arm64 doesn't have a 'cpuid' instruction equivalent and relies on
+		// HWCAP/HWCAP2 bits for hardware capabilities.
+		cpu.HWCap = uint(val)
+	case _AT_HWCAP2:
+		cpu.HWCap2 = uint(val)
+	}
+}
 
 //go:nosplit
 func cputicks() int64 {
